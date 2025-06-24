d781ae434aafdd546b0d26fa1b788d57329e44df
Kaga Akatsuki <filberttkarry2210@gmail.com>
2025-06-24 14:53:23 +0700

    .

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,7 @@ const AUTH_CACHE_DURATION = 30; // seconds
 const authCache = new Map();
 
 export async function middleware(request) {
+    console.log(request.headers)
     if (request.headers.get("next-router-prefetch")) {
         return NextResponse.next();
     }
